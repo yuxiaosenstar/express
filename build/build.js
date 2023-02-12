69392a2ac7ae1f938692d78d179a513d3ca33fe6
yuchangchang <yuchang251314@163.com>
2023-02-12 15:36:21 +0800

    feat:修改docker镜像名

--- build/build.js
+++ build/build.js
@@ -1,7 +1,7 @@
-const { exec } = require("child_process");
-const dockerImageName = "yuxiaosen/koa-web-app";
+const { exec } = require("child_process")
+const dockerImageName = "yuxiaosen/mshop-serve"
 
 exec(`docker buildx build \
 --platform linux/amd64,linux/arm64 \
 --push -t ${dockerImageName} .
-`);
+`)
